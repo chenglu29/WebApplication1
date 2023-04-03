6e47d1a155274cd98c1be3ebb60558637dcf97fe
Lu Cheng <75212732+chenglu29@users.noreply.github.com>
2023-04-03 18:28:40 -0400

    Add files via upload

---
+++ Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
+
+namespace WebApplication1.Pages
+{
+    public class IndexModel : PageModel
+    {
+
+        public string fn;
+        public void OnGet()
+        {
+            string connectionString = "Data Source=LU-CHENG\\SQLEXPRESS;Initial Catalog=test;Integrated Security=True;TrustServerCertificate=true";
+            SqlConnection con = new SqlConnection(connectionString);
+            con.Open();
+            string sqlQuery = "select firstname from Table_1";
+            SqlCommand cmd = new SqlCommand(sqlQuery, con);
+            SqlDataReader dr = cmd.ExecuteReader();
+            if (dr.Read()) {
+                fn = dr["firstname"].ToString();
+
+            }
+            con.Close();
+        }
+    }
+}
